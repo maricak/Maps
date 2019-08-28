d0b83657720cd5b4baf3f99ff70e2a3fe87bc22f
Marija Kostić <marijakostic25@gmail.com>
2019-08-28 22:22:53 +0200

    Add HasChart.

--- Maps.Data/Migrations/201908281538200_RemoveHasData.cs
+++ Maps.Data/Migrations/201908281538200_RemoveHasData.cs
@@ -1,15 +1,14 @@
 namespace Maps.Data.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class RemoveHasData : DbMigration
     {
         public override void Up()
         {
             DropColumn("dbo.Layer", "HasData");
         }
-        
+
         public override void Down()
         {
             AddColumn("dbo.Layer", "HasData", c => c.Boolean(nullable: false));
---
+++ Maps.Data/Migrations/201908282022141_AddHasChart.cs
@@ -0,0 +1,18 @@
+namespace Maps.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddHasChart : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Column", "HasChart", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Column", "HasChart");
+        }
+    }
+}
